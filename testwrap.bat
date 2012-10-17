356ed2790c06bec6fc3e2a6102b10623a8836110
actionfarsi <actionfarsi@gmail.com>
2012-10-17 15:39:25 -0400

    Signed-off-by: actionfarsi <actionfarsi@gmail.com>

---
+++ testwrap.bat
@@ -0,0 +1,6 @@
+REM Testing Unwrap-wraproutines
+
+release\Panorama sphrWarp images\pano1_0008.tga images\t_warp1.tga 595 -0.15 0.0
+
+REM Test align
+release\Panorama alignPair images\warp08.key images\warp09.key images\match-08-09.txt 200 4 sift
